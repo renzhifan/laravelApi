ffb69f0513994098441035308b9a8947e7dc7f87
renzhifan <zhifan6797@163.com>
2019-12-04 14:09:14 +0800

    解决跨域问题

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 
-Route::namespace('Api')->prefix('v1')->group(function () {
+Route::namespace('Api')->prefix('v1')->middleware('cors')->group(function () {
     Route::get('/users','UserController@index')->name('users.index');
     Route::get('/users/{user}','UserController@show')->name('users.show');
     Route::post('/users','UserController@store')->name('users.store');
