d471ede1b22b70bc7c46ac01b18895eb9b899470
renzhifan <zhifan6797@163.com>
2019-12-04 15:02:36 +0800

    统一Api-Resource资源

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,19 +4,19 @@ namespace App\Http\Controllers\Api;
 use App\Http\Requests\Api\UserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Resources\Api\UserResource;
 use Illuminate\Support\Facades\Auth;
 class UserController extends Controller
 {
 
     //返回用户列表
     public function index(){
-        //3个用户为一页
         $users = User::paginate(3);
-        return $this->success($users);
+        return UserResource::collection($users);
     }
     //返回单一用户信息
     public function show(User $user){
-        return $this->success($user);
+        return $this->success(new UserResource($user));
     }
     //用户注册
     public function store(UserRequest $request){
