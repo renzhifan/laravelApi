3d516da51da1d090cee75951f5d76321a9ebb86c
renzhifan <zhifan6797@163.com>
2019-12-04 14:03:45 +0800

    编写相关资源接口和路由

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -9,23 +9,26 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\Api\UserRequest;
 class UserController extends Controller
 {
-    public function index()
-    {
-        return 'index';
+    //返回用户列表
+    public function index(){
+        //3个用户为一页
+        $users = User::paginate(3);
+        return $users;
     }
-
+    //返回单一用户信息
+    public function show(User $user){
+        return $user;
+    }
     //用户注册
-    public function store(UserRequest $request)
-    {
+    public function store(UserRequest $request){
         User::create($request->all());
         return '用户注册成功。。。';
-    }
 
+    }
     //用户登录
-    public function login(Request $request)
-    {
-        $res = Auth::guard('web')->attempt(['name' => $request->name, 'password' => $request->password]);
-        if ($res) {
+    public function login(Request $request){
+        $res=Auth::guard('web')->attempt(['name'=>$request->name,'password'=>$request->password]);
+        if($res){
             return '用户登录成功...';
         }
         return '用户登录失败';
