961c7a59af0e90f1f278e200bf0af89a35b9e1ae
renzhifan <zhifan6797@163.com>
2019-12-05 09:15:19 +0800

    自动刷新用户认证

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,18 @@
 use Illuminate\Http\Request;
 
 Route::namespace('Api')->prefix('v1')->middleware('cors')->group(function () {
-    Route::get('/users','UserController@index')->name('users.index');
-    Route::get('/users/{user}','UserController@show')->name('users.show');
+    //用户注册
     Route::post('/users','UserController@store')->name('users.store');
+    //用户登录
     Route::post('/login','UserController@login')->name('users.login');
-    //当前用户信息
-    Route::post('/users/info','UserController@info')->name('users.info');
-
+    Route::middleware('api.refresh')->group(function () {
+        //当前用户信息
+        Route::post('/users/info','UserController@info')->name('users.info');
+        //用户列表
+        Route::get('/users','UserController@index')->name('users.index');
+        //用户信息
+        Route::get('/users/{user}','UserController@show')->name('users.show');
+        //用户退出
+        Route::get('/logout','UserController@logout')->name('users.logout');
+    });
 });
